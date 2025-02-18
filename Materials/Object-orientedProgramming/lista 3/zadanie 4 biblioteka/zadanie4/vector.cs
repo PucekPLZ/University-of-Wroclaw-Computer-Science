a8eaa4ba1d6c4aa72b39d6230b4ec93528de970f
PucekPLZ <44066546+PucekPLZ@users.noreply.github.com>
2025-02-18 15:26:15 +0100

    Object-oriented Programming list 3 ex 4

---
+++ Materials/Object-orientedProgramming/lista 3/zadanie 4 biblioteka/zadanie4/vector.cs
@@ -0,0 +1,94 @@
+﻿namespace zadanie4;
+public class Wektor
+{
+    public float[] vec = Array.Empty<float>();
+
+    public Wektor(int n)
+    {
+        vec = new float[n];
+    }
+
+    public void fillVector(float[] elements)
+    {
+        for (int i = 0; i < vec.Length; i++)
+        {
+            vec[i] = elements[i];
+        }
+    }
+
+    public void printVector()
+    {
+        for (int i = 0; i < vec.Length; i++)
+        {
+            Console.Write(vec[i] + " ");
+        }
+    }
+
+    public Wektor addVectors(Wektor other)
+    {
+        if (vec.Length != other.vec.Length)
+        {
+            throw new ArgumentException("Vectors have diffrent dimensions");
+        }
+        else
+        {
+            Wektor result = new Wektor(vec.Length);
+
+            for (int i = 0; i < vec.Length; i++)
+            {
+                result.vec[i] = vec[i] + other.vec[i];
+            }
+
+            return result;
+        }
+        
+    }
+
+    public Wektor scalarProduct(Wektor other)
+    {
+        if (vec.Length != other.vec.Length)
+        {
+            throw new ArgumentException("Vectors have diffrent dimensions");
+        }
+        else
+        {
+            Wektor result = new Wektor(vec.Length);
+
+            for (int i = 0; i < vec.Length; i++)
+            {
+                result.vec[i] = vec[i] * other.vec[i];
+            }
+
+            return result;
+        }
+    }
+
+    public Wektor scalar(float alfa)
+    {
+        Wektor result = new Wektor(vec.Length);
+
+        for (int i = 0; i < vec.Length; i++)
+        {
+            result.vec[i] = vec[i] * alfa;
+        }
+
+        return result;
+    }
+
+    public float norma()
+    {
+        float len = 0;
+        Wektor vec2 = new Wektor(vec.Length);
+        vec2.fillVector(vec);
+        vec2.scalarProduct(vec2);
+
+        for (int i = 0; i < vec.Length; i++)
+        {
+            len += vec2.vec[i];
+        }
+
+        len = MathF.Sqrt(len);
+        return len;
+    }
+}
+
---
+++ Materials/Object-orientedProgramming/lista 3/zadanie 4 wektor/wektor/Program.cs
@@ -0,0 +1,39 @@
+﻿// Lucjan Pucelak
+// Visual Studio
+
+using zadanie4;
+using System;
+
+namespace zadanie4wektor
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Wektor vector1 = new Wektor(3);
+            vector1.fillVector(new float[] { 1, 2, 3 });
+
+            Wektor vector2 = new Wektor(3);
+            vector2.fillVector(new float[] { 1, 2, 3 });
+
+            Console.Write(vector1.norma());
+            Console.WriteLine();
+
+            vector1.printVector();
+            Console.WriteLine();
+
+            Wektor sum = vector1.addVectors(vector2);
+            sum.printVector();
+            Console.WriteLine();
+
+            Wektor sp = vector1.scalarProduct(vector2);
+            sp.printVector();
+            Console.WriteLine();
+
+            Wektor multScalar = vector1.scalar(5);
+            multScalar.printVector();
+        }
+    }
+}
+
+
