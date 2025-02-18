ae5ca04c859f7d026192055c53cd9a14233a924f
PucekPLZ <44066546+PucekPLZ@users.noreply.github.com>
2025-02-18 15:27:15 +0100

    Object-oriented Programming list 3 ex 1

---
+++ Materials/Object-orientedProgramming/lista 3/zadanie1biblioteka/zadanie1biblioteka/Class1.cs
@@ -0,0 +1,136 @@
+﻿namespace zadanie1biblioteka;
+public class Lista<T>
+{
+    class Element
+    {
+        public T value;
+        public Element next;
+        public Element previous;
+
+        public Element(T value)
+        {
+            this.next = null;
+            this.previous = null;
+            this.value = value;
+        }
+    }
+
+    Element first;
+
+    public Lista()
+    {
+        first = null;
+    }
+
+    public void push_front(T elem)
+    {
+        Element newElement = new Element(elem);
+
+        if (first == null)
+        {
+            first = newElement;
+        }
+        else if (first.next == null)
+        {
+            newElement.previous = first;
+            newElement.next = first;
+            first.previous = newElement;
+            first = newElement;
+        }
+        else
+        {
+            newElement.next = first;
+            newElement.previous = first.previous;
+            first.previous = newElement;
+            first = newElement;
+        }
+    }
+
+    public void push_back(T elem)
+    {
+        Element newElement = new Element(elem);
+
+        if (first == null)
+        {
+            first = newElement;
+        }
+        else if (first.next == null)
+        {
+            first.previous = newElement;
+            first.next = newElement;
+            newElement.previous = first;
+        }
+        else
+        {
+            first.previous.next = newElement;
+            newElement.previous = first.previous;
+            first.previous = newElement;
+        }
+    }
+
+    public T pop_front()
+    {
+        if (first == null)
+        {
+            throw new NullReferenceException("Lista is empty");
+        }
+        else if (first.next == null)
+        {
+            T valueDel = first.value;
+            first = null;
+            return valueDel;
+        }
+        else
+        {
+            T valueDel = first.value;
+            first.next.previous = first.previous;
+            first = first.next;
+            return valueDel;
+        }
+    }
+
+    public T pop_back()
+    {
+        if (first == null)
+        {
+            throw new NullReferenceException("Lista is empty");
+        }
+        else if (first.next == null)
+        {
+            T valueDel = first.value;
+            first = null;
+            return valueDel;
+        }
+        else
+        {
+            T valueDel = first.previous.value;
+            first.previous.previous.next = null;
+            first.previous = first.previous.previous;
+            return valueDel;
+        }
+    }
+
+    public void printLista()
+    {
+        Element temp = first;
+        while (temp != null)
+        {
+            Console.Write(temp.value + " ");
+            temp = temp.next;
+        }
+    }
+
+    public bool is_empty()
+    {
+        if (first == null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+
---
+++ Materials/Object-orientedProgramming/lista 3/zadanie1lista/zadanie1lista/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using zadanie1biblioteka;
+
+namespace zadanie1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Lista<int> llista = new Lista<int>();
+
+            llista.push_back(1);
+            llista.push_back(2);
+            llista.push_back(3);
+            llista.printLista();
+            Console.WriteLine();
+
+            llista.push_front(4);
+            llista.push_front(5);
+            llista.push_front(6);
+            llista.printLista();
+            Console.WriteLine();
+
+            llista.pop_front();
+            llista.pop_front();
+            llista.pop_front();
+            llista.printLista();
+            Console.WriteLine();
+
+            Console.WriteLine(llista.is_empty());
+
+            llista.pop_back();
+            llista.pop_back();
+            llista.pop_back();
+            llista.printLista();
+
+            Console.WriteLine(llista.is_empty());
+        }
+    }
+}
+
+
+
+
